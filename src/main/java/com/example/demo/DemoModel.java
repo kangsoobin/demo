0ccdb145abf327c172593101f7afe727e70b6929
Kang Soo Bin <eqsubin@sookmyung.ac.kr>
2023-06-17 15:37:44 +0900

    백엔드 개발 10 @PathVariable 사용하기 추가

---
+++ src/main/java/com/example/demo/DemoModel.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+public class DemoModel {
+    // 클래스 내부에 필요한 멤버 변수, 메서드 등을 정의할 수 있습니다.
+    // 예시:
+    private String name;
+
+    public DemoModel(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
